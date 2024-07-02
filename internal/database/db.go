d41c0f9c5bdeb71a0d40f58dd18dded2778aabac
Ibka Anhar Fatcha <ibkaanhar1@gmail.com>
2024-07-03 04:22:17 +0700

    feat: create database if not exist on first run

--- internal/database/db.go
+++ internal/database/db.go
@@ -11,12 +11,24 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN: fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", configs.GetDBUSER(), configs.GetDBPASS(), configs.GetDBHOST(), configs.GetDBPORT(), configs.GetDBNAME()),
-	}), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(
+		fmt.Sprintf("%v:%v@tcp(%v:%v)/", configs.GetDBUSER(), configs.GetDBPASS(), configs.GetDBHOST(), configs.GetDBPORT()),
+	), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
+	}
+
+	_ = db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %v;", configs.GetDBNAME()))
+
+	normalDSN := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", configs.GetDBUSER(), configs.GetDBPASS(), configs.GetDBHOST(), configs.GetDBPORT(), configs.GetDBNAME())
+	db, err = gorm.Open(
+		mysql.Open(normalDSN),
+		&gorm.Config{},
+	)
+
+	if err != nil {
+		panic(err.Error())
 	}
 
 	DB = db
